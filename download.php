1e4ad7b781468b2ed77d277f216be6b30960ea1f
wkeil <wkeil>
2011-05-24 15:50:47 +0000

    ASSIGNED - bug 342070: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=342070

--- download.php
+++ download.php
@@ -19,8 +19,8 @@
 
 <h3>
 <h3>Eclipse UOMo 0.6 RC2</h3>
-<p><b>Update site:</b> http://www.eclipse.org/uomo/updates/<br/>
-<b>ZIP file: </b><a href="/uomo/downloads/uomo-0.6.0.RC2.zip">uomo-0.6.0.RC2.zip</a> (590 kB)</p>
+<p><b>Update site:</b> http://download.eclipse.org/uomo/<br/>
+<!-- b>ZIP file: </b><a href="/uomo/downloads/uomo-0.6.0.RC2.zip">uomo-0.6.0.RC2.zip</a> (590 kB)</p -->
 
 <!-- h3>Indigo - Eclipse 3.7 (unreleased)</h3>
 <p><b>Update site:</b> http://download.eclipse.org/myproject/<br/>
