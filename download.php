3210a4c9bdb80d7dc96be45b80dc9fcbb060257c
wkeil <wkeil>
2011-08-31 00:22:24 +0000

    ASSIGNED - bug 350289: Activator invalid when sample Eclipse app started https://bugs.eclipse.org/bugs/show_bug.cgi?id=350289

--- download.php
+++ download.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009 
+ * Copyright (c) 2009, 2011 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -21,6 +21,9 @@
 <h3>Eclipse UOMo 0.6 RC2</h3>
 <p><b>Update site:&nbsp</b><a href="http://download.eclipse.org/uomo/rc2/">http://download.eclipse.org/uomo/rc2/</a><br/>
 <!-- b>ZIP file: </b><a href="/uomo/downloads/uomo-0.6.0.RC2.zip">uomo-0.6.0.RC2.zip</a> (590 kB)</p -->
+
+<h3>Eclipse UOMo 0.6 RC3</h3>
+<p><b>Update site:&nbsp</b><a href="http://download.eclipse.org/uomo/rc3/">http://download.eclipse.org/uomo/rc3/</a><br/>
 
 <!-- h3>Indigo - Eclipse 3.7 (unreleased)</h3>
 <p><b>Update site:</b> http://download.eclipse.org/myproject/<br/>
