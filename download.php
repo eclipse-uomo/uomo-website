8f69885c95a613fef67c070398a431d457e582f8
wkeil <wkeil>
2011-10-09 04:00:55 +0000

    ASSIGNED - bug 342070: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=342070

--- download.php
+++ download.php
@@ -9,47 +9,12 @@
  * Contributors:
  *    
  *******************************************************************************/
-
+    
 	$pageTitle 		= "UOMo - Download";
 
-	$html  = <<<EOHTML
-<div id="bigbuttons">
-</div>
-
-<div id="midcolumn">
-<h2>$pageTitle</h2>
-<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
-
-<!-- h3>
-<h3>Eclipse UOMo 0.6 RC2</h3>
-<p><b>Update site:&nbsp</b><a href="http://download.eclipse.org/uomo/rc2/">http://download.eclipse.org/uomo/rc2/</a><br/ -->
-<!-- b>ZIP file: </b><a href="/uomo/downloads/uomo-0.6.0.RC2.zip">uomo-0.6.0.RC2.zip</a> (590 kB)</p -->
-
-<h3>Eclipse UOMo 0.6 RC3 (Incubation)</h3>
-<p><b>Update site:&nbsp</b><a href="http://download.eclipse.org/uomo/rc3/">http://download.eclipse.org/uomo/rc3/</a><br/>
-
-<!-- h3>Indigo - Eclipse 3.7 (unreleased)</h3>
-<p><b>Update site:</b> http://download.eclipse.org/myproject/<br/>
-<b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p -->
-
-
-</div>
-
-<div id="rightcolumn">
-
-  <div>
-  <h3>Incubation</h3>
-  <center>
-    <a href="http://www.eclipse.org/projects/what-is-incubation.php">
-      <img src="http://www.eclipse.org/images/egg-incubation.png" border="0" alt="Incubation"/>
-    </a>
-  </center>
-  <p>UOMo is in the <a href="http://www.eclipse.org/projects/dev_process/incubation-phase.php">Incubation Phase</a>.</p>
-  </div>
-  
-</div>
-
-EOHTML;
+    //  # Paste your HTML content between the EOHTML markers!
+    $html = file_get_contents('pages/_download.html');
+    
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
