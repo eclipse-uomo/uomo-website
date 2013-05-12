235e3aebcf10d91097edcebf14e4a893d5c32b6c
keilw <werner.keil@gmx.net>
2013-05-12 16:27:17 +0200

    ASSIGNED - bug 342070: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=342070

--- download.php
+++ download.php
@@ -1,6 +1,6 @@
 <?php                                                                                                                       require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();       include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009, 2011 
+ * Copyright (c) 2009, 2013 
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -25,8 +25,10 @@
 <p><b>Update site:&nbsp</b><a href="http://download.eclipse.org/uomo/rc2/">http://download.eclipse.org/uomo/rc2/</a><br/ -->
 <!-- b>ZIP file: </b><a href="/uomo/downloads/uomo-0.6.0.RC2.zip">uomo-0.6.0.RC2.zip</a> (590 kB)</p -->
 
+<!--
 <h3>Eclipse UOMo 0.6 RC3 (Incubation)</h3>
 <p><b>Update site:&nbsp</b> http://download.eclipse.org/uomo/rc3/<br/>
+-->
 
 <h3>Eclipse UOMo 0.6 RC4 (Incubation)</h3>
 <p><b>Update site:&nbsp</b> http://download.eclipse.org/uomo/rc4/<br/>
