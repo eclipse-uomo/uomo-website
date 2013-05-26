731c09611984e7d7281173750997a6f410765cf3
keilw <werner.keil@gmx.net>
2013-05-26 15:18:49 +0200

    ASSIGNED - bug 342070: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=342070

--- download.php
+++ download.php
@@ -1,62 +1,33 @@
-<?php                                                                                                                       require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App    = new App();    $Nav    = new Nav();    $Menu   = new Menu();       include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009, 2013 
+ * Copyright (c) 2010, 2013 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Werner Keil
+ *		Eclipse Foundation - Initial version
+ *		Werner Keil - Changes for UOMo
  *******************************************************************************/
 
-    $pageTitle      = "UOMo - Download";
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
+	$App = new App();
+	$Nav = new Nav();
+	$Menu = new Menu();
+	include($App->getProjectCommon());
+	
+	$localVersion = false;
+	
+	# Define these here, or in _projectCommon.php for site-wide values
+	# $pageKeywords	= "eclipse, project";
+	# $pageAuthor	= "Werner Keil";
+	$pageTitle 		= "UOMo &ndash; Download";
 
-    $html  = <<<EOHTML
-<div id="bigbuttons">
-</div>
+	$html = file_get_contents('pages/_download.html');
 
-<div id="midcolumn">
-<h2>$pageTitle</h2>
-<p>All downloads are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
-
-<h3>Eclipse UOMo 0.6 (Incubation)</h3>
-<p><b>Update site:&nbsp</b> http://download.eclipse.org/uomo/0.6/repository/<br/>
-
-<p><b>Repository Archive to install Offline:&nbsp</b><a href="http://download.eclipse.org/uomo/0.6/eclipse-repository-0.6.0-SNAPSHOT.zip">eclipse-repository-0.6.0-SNAPSHOT.zip</a><br/>
-Use this, if you are behind a <b>firewall</b>, have <b>proxy</b> problems with update sites in your corporate network of face similar issues.<br/>
-After downloading the archive, select it under "Install New Software" > "Add Repository" > "Archive..." and use as offline repository.
-</p>
-
-<h3>Eclipse UOMo 0.6 for Kepler (Incubation)</h3>
-<p><b>Update site:&nbsp</b> http://download.eclipse.org/uomo/0.6/kepler/repository/<br/>
-
-<p><b>Repository Archive to install Offline:&nbsp</b><a href="http://download.eclipse.org/uomo/0.6/kepler/eclipse-repository-0.6.0-SNAPSHOT.zip">eclipse-repository-0.6.0-SNAPSHOT.zip</a><br/>
-Use this, if you are behind a <b>firewall</b>, have <b>proxy</b> problems with update sites in your corporate network of face similar issues.<br/>
-After downloading the archive, select it under "Install New Software" > "Add Repository" > "Archive..." and use as offline repository.
-</p>
-
-<!-- h3>Indigo - Eclipse 3.7 (unreleased)</h3>
-<p><b>Update site:</b> http://download.eclipse.org/myproject/<br/>
-<b>ZIP file: </b><a href="/downloads/download.php?file=/myproject/file.zip">file.zip</a> (10 MiB)</p -->
-
-
-
-</div>
-
-<div id="rightcolumn">
-
-  <div>
-  <h3>Incubation</h3>
-  <center>
-<div class="project_state"><a class="incubating" href="http://www.eclipse.org/projects/what-is-incubation.php"></a></div></div>
-  </center>
-  <p>UOMo is in the <a href="http://www.eclipse.org/projects/dev_process/incubation-phase.php">Incubation Phase</a>.</p>
-  </div>
-  
-</div>
-
-EOHTML;
-    # Generate the web page
-    $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
