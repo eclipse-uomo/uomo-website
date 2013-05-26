adb7ef296775f6208276cfea254fb594061d2e91
keilw <werner.keil@gmx.net>
2013-05-26 15:24:35 +0200

    ASSIGNED - bug 342070: Update Website https://bugs.eclipse.org/bugs/show_bug.cgi?id=342070

--- download.php
+++ download.php
@@ -1,33 +1,29 @@
-<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+<?php
 /*******************************************************************************
- * Copyright (c) 2010, 2013 Eclipse Foundation and others.
+ * Copyright (c) 2009 Eclipse Foundation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *		Eclipse Foundation - Initial version
- *		Werner Keil - Changes for UOMo
+ *    Werner Keil
  *******************************************************************************/
 
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");
-	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");
-	$App = new App();
-	$Nav = new Nav();
-	$Menu = new Menu();
-	include($App->getProjectCommon());
+	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());
 	
 	$localVersion = false;
 	
 	# Define these here, or in _projectCommon.php for site-wide values
-	# $pageKeywords	= "eclipse, project";
-	# $pageAuthor	= "Werner Keil";
-	$pageTitle 		= "UOMo &ndash; Download";
-
+	$pageKeywords	= "eclipse, project, uomo, units, measurement, ucum";
+	$pageAuthor		= "Werner Keil";
+	$pageTitle 		= "UOMo Download";
+	
+	
+	// 	# Paste your HTML content between the EOHTML markers!
 	$html = file_get_contents('pages/_index.html');
 
 	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
+
 ?>
