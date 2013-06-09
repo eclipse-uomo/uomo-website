6b15fa5f886a3a1c75a3fca542255537b6d6ca10
keilw <werner.keil@gmx.net>
2013-06-09 23:19:22 +0200

    ASSIGNED - bug 332369: Update Documentation https://bugs.eclipse.org/bugs/show_bug.cgi?id=332369

--- documentation.php
+++ documentation.php
@@ -21,7 +21,7 @@
 <p>
 <a href="/uomo/documents/uomo-0.6.pdf">UOMo 0.6 Reference</a> (PDF: 471 kB)</p>
 
-<a href="/uomo/documents/EclipseUOMoTutorial.pdf">UOMo Tutorial</a> (PDF: 1.141 MB)</p>
+<a href="/uomo/documents/EclipseUOMoTutorial.pdf">Eclipse UOMo Tutorial</a> (PDF: 1.14 MB)</p>
 
 <h3>Presentations</h3>
 <iframe src="http://www.slideshare.net/slideshow/embed_code/21949160" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://de.slideshare.net/keilw/geecon-2013-standards-for-the-future-of-java-embedded" title="geecon 2013 - Standards for the Future of Java Embedded" target="_blank">geecon 2013 - Standards for the Future of Java Embedded</a> </strong> from <strong><a href="http://de.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
