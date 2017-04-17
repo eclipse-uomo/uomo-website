c0db2b14328b6b5dc0fb75fefd4e977a768fa7f2
Werner Keil <werner.keil@gmx.net>
2017-04-17 05:06:15 +0200

    342070: Update Website
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=342070

--- documentation2.php
+++ documentation2.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009, 2013
+ * Copyright (c) 2009, 2017
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
