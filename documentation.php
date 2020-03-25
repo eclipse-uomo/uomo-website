280e3967e1a707ea87b56a5d0dcb35f95f9dd617
Werner Keil <werner.keil@gmx.net>
2020-03-25 23:57:09 +0100

    Updated docs

--- documentation.php
+++ documentation.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009, 2013
+ * Copyright (c) 2009, 2020
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,6 +15,8 @@
 	$html  = <<<EOHTML
 <div id="midcolumn">
 <h2>$pageTitle</h2>
+<p>
+<p>
 <p>All documents are provided under the terms and conditions of the <a href="/legal/epl/notice.php">Eclipse Foundation Software User Agreement</a> unless otherwise specified.</p>
 
 <h3>Documents</h3>
@@ -24,21 +26,18 @@
 <a href="/uomo/documents/EclipseUOMoTutorial.pdf">Eclipse UOMo Tutorial</a> (PDF: 2 MB)</p>
 
 <h3>Presentations</h3>
+
+<iframe src="//www.slideshare.net/slideshow/embed_code/key/oaN7Bu57Ssrs2g" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/keilw/how-jsr-385-could-have-saved-the-mars-climate-orbiter-jfokus-2020" title="How JSR 385 could have Saved the Mars Climate Orbiter - JFokus 2020" target="_blank">How JSR 385 could have Saved the Mars Climate Orbiter - JFokus 2020</a> </strong> from <strong><a href="https://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
+<iframe src="//www.slideshare.net/slideshow/embed_code/key/7Vz8LGBtx32w3L" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/keilw/how-jsr-385-could-have-saved-the-mars-climate-orbiter-adoptajsr-day" title="How JSR 385 could have Saved the Mars Climate Orbiter - Adopt-a-JSR Day" target="_blank">How JSR 385 could have Saved the Mars Climate Orbiter - Adopt-a-JSR Day</a> </strong> from <strong><a href="https://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
+<iframe src="//www.slideshare.net/slideshow/embed_code/key/CXmgcYpD2OOq2r" width="595" height="485" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="//www.slideshare.net/keilw/physikal-using-kotlin-for-clean-energy-kug-munich" title="Physikal - Using Kotlin for Clean Energy - KUG Munich" target="_blank">Physikal - Using Kotlin for Clean Energy - KUG Munich</a> </strong> from <strong><a href="https://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
 <iframe src="//www.slideshare.net/slideshow/embed_code/37436238" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC; border-width:1px; margin-bottom:5px; max-width: 100%;" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="https://de.slideshare.net/keilw/eclipse-democamp-vienna-iot-und-science-wg" title="Eclipse DemoCamp Vienna: IoT, M2M fuer Fitness, Gesundheit und Wissenschaft" target="_blank">Eclipse DemoCamp Vienna: IoT, M2M fuer Fitness, Gesundheit und Wissenschaft</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
 
 <iframe src="http://www.slideshare.net/slideshow/embed_code/27347037" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="https://de.slideshare.net/keilw/m4m-2-the-rescue-of-m2m-nordic-edition" title="M4M 2 the Rescue of M2M (Eclipse DemoCamp Trondheim)" target="_blank">M4M 2 the Rescue of M2M (Eclipse DemoCamp Trondheim)</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
 
 <iframe src="http://www.slideshare.net/slideshow/embed_code/21949160" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://de.slideshare.net/keilw/geecon-2013-standards-for-the-future-of-java-embedded" title="geecon 2013 - Standards for the Future of Java Embedded" target="_blank">geecon 2013 - Standards for the Future of Java Embedded</a> </strong> from <strong><a href="http://de.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
-
-<iframe src="http://www.slideshare.net/slideshow/embed_code/21248130" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://de.slideshare.net/keilw/m4m-2-the-rescue-of-m2m" title="M4M 2 the Rescue of M2M" target="_blank">M4M 2 the Rescue of M2M</a> </strong> from <strong><a href="http://de.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
-
-<iframe src="http://www.slideshare.net/slideshow/embed_code/20131503" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://de.slideshare.net/keilw/standards-for-the-future-of-java-embedded-169" title="Standards for the Future of Java Embedded (16:9)" target="_blank">Standards for the Future of Java Embedded (16:9)</a> </strong> from <strong><a href="http://de.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
-
-<iframe src="http://www.slideshare.net/slideshow/embed_code/15329470" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/the-eclipse-m2m-iwg-and-standards-for-the-internet-of-things" title="The Eclipse M2M IWG and Standards for the Internet of Things" target="_blank">The Eclipse M2M IWG and Standards for the Internet of Things</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
-
-<iframe src="http://www.slideshare.net/slideshow/embed_code/14605795" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/standards-for-the-future-of-java-embedded" title="Standards for the Future of Java Embedded" target="_blank">Standards for the Future of Java Embedded</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
-
-<iframe src="http://www.slideshare.net/slideshow/embed_code/14436908" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/the-ohf-legacy" title="The OHF Legacy" target="_blank">The OHF Legacy</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
 
 <p>
 <p>
--- documentation2.php
+++ documentation2.php
@@ -1,6 +1,6 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 /*******************************************************************************
- * Copyright (c) 2009, 2017
+ * Copyright (c) 2009, 2020
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -20,6 +20,17 @@
 <a href="documentation.php">Recent documentation</a>
 
 <h3>Earlier presentations</h3>
+<p>
+
+<iframe src="http://www.slideshare.net/slideshow/embed_code/21248130" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://de.slideshare.net/keilw/m4m-2-the-rescue-of-m2m" title="M4M 2 the Rescue of M2M" target="_blank">M4M 2 the Rescue of M2M</a> </strong> from <strong><a href="http://de.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
+<iframe src="http://www.slideshare.net/slideshow/embed_code/20131503" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://de.slideshare.net/keilw/standards-for-the-future-of-java-embedded-169" title="Standards for the Future of Java Embedded (16:9)" target="_blank">Standards for the Future of Java Embedded (16:9)</a> </strong> from <strong><a href="http://de.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
+<iframe src="http://www.slideshare.net/slideshow/embed_code/15329470" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen webkitallowfullscreen mozallowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/the-eclipse-m2m-iwg-and-standards-for-the-internet-of-things" title="The Eclipse M2M IWG and Standards for the Internet of Things" target="_blank">The Eclipse M2M IWG and Standards for the Internet of Things</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
+<iframe src="http://www.slideshare.net/slideshow/embed_code/14605795" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/standards-for-the-future-of-java-embedded" title="Standards for the Future of Java Embedded" target="_blank">Standards for the Future of Java Embedded</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
+
+<iframe src="http://www.slideshare.net/slideshow/embed_code/14436908" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/the-ohf-legacy" title="The OHF Legacy" target="_blank">The OHF Legacy</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
 
 <iframe src="http://www.slideshare.net/slideshow/embed_code/13360361" width="597" height="486" frameborder="0" marginwidth="0" marginheight="0" scrolling="no" style="border:1px solid #CCC;border-width:1px 1px 0;margin-bottom:5px" allowfullscreen> </iframe> <div style="margin-bottom:5px"> <strong> <a href="http://www.slideshare.net/keilw/typesafe-dsls" title="Type-safe DSLs" target="_blank">Type-safe DSLs</a> </strong> from <strong><a href="http://www.slideshare.net/keilw" target="_blank">Werner Keil</a></strong> </div>
 
