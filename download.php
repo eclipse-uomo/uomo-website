96648f69d6670d3372d2c10af8f23c755effa5c4
Werner Keil <werner.keil@gmx.net>
2023-12-11 23:46:34 +0100

    Fixed download links Signed-off-by: Werner Keil <werner.keil@gmx.net>

--- download.php
+++ download.php
@@ -37,7 +37,7 @@
 	<h4>Eclipse UOMo 0.8 (Incubation)</h4>
 	<p><b>1. Unit Systems Update Site:&nbsp</b> https://download.eclipse.org/uomo/dependency/uom-systems/1.3a/<br/>
 	<p><b>2. UOMo Update site:&nbsp</b> http://download.eclipse.org/uomo/0.8/repository/<br/>
-	<p><b>&nbsp&nbsp&nbspRepository Archive to install Offline:&nbsp</b><a href="http://download.eclipse.org/uomo/0.8/eclipse-repository-0.8.0-SNAPSHOT.zip">eclipse-repository-0.8.0-SNAPSHOT.zip</a><br/>
+	<p><b>&nbsp&nbsp&nbspRepository Archive to install Offline:&nbsp</b><a href="http://download.eclipse.org/uomo/0.8/eclipse-repository-0.8.0.zip">eclipse-repository-0.8.0.zip</a><br/>
 	</p>	
 	
 	<h4>Eclipse UOMo 0.7 (Incubation)</h4>
